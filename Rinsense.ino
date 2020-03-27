df46c4b2090d1ed79bf0ad1b76b6021eb00d40dd
Jasdoge <jasdoge@gmail.com>
2020-03-27 23:56:33 +0100

    write a basic program

--- Rinsense.ino
+++ Rinsense.ino
@@ -1,7 +1,7 @@
 
 // PIN MAPPING
-const byte PIN_RED = 1;
-const byte PIN_GREEN = 0;
+const byte PIN_RED = 0;
+const byte PIN_GREEN = 1;
 
 const byte PIN_IR_OUT = 2;
 const byte PIN_IR_IN = 3;
@@ -13,7 +13,11 @@ const byte STATE_TIMER = 2;			// Hands removed, blink the red LEDs on and off in
 const byte STATE_DONE = 3;			// All done! Glow green! Good job!
 byte STATE = STATE_TRACKING;
 
+const byte MAX_TICKS = 40;			// Ticking at 2hz so 40 = 20 sec
+byte ticks;							// Nr 0.5s blinks done so far
 
+
+// Todo: Add beeper
 
 
 void setup(){
@@ -43,15 +47,57 @@ void setup(){
 
 void loop(){
 
-	digitalWrite(PIN_IR_OUT, HIGH);
-	delayMicroseconds(10);
-	bool near = digitalRead(PIN_IR_IN);
-	digitalWrite(PIN_IR_IN, LOW);
+	// Find presence of hands
+	if( STATE == STATE_TRACKING || STATE == STATE_FOUND ){
 
-	digitalWrite(PIN_GREEN, near);
+		digitalWrite(PIN_IR_OUT, HIGH);
+		delayMicroseconds(10);
+		bool near = digitalRead(PIN_IR_IN);
+		digitalWrite(PIN_IR_IN, LOW);
 
-	// Todo: Sleep
-	delay(100);
+		if( near && STATE == STATE_TRACKING ){
+			
+			// Turn on half red
+			++STATE;
+			analogWrite(PIN_RED, 50);
+
+		}
+		else if( !near && STATE == STATE_FOUND ){
+			
+			++STATE;
+			ticks = 0;	// reset ticks
+
+		}
+
+		// State might have moved on beyond these, which is why it's an else if instead of else
+		if( STATE == STATE_TRACKING )
+			delay(1000);							// Todo: Replace with sleep
+		else if( STATE == STATE_FOUND )
+			delay(100);								// Todo: Replace with sleep
+
+	}
+
+	if( STATE == STATE_TIMER ){
+
+		++ticks;
+		digitalWrite(PIN_RED, !(ticks%2) );			// Tick on even
+		delay(500);									// Todo: Sleep?
+
+		// Finished ticking
+		if( ticks >= MAX_TICKS )
+			++STATE;
+
+	}
+
+	if( STATE == STATE_DONE ){
+
+		digitalWrite(PIN_RED, LOW);
+		digitalWrite(PIN_GREEN, HIGH);
+		delay(3000);								// Todo: sleep?
+		STATE = STATE_TRACKING;	 // Reset to basic
+		digitalWrite(PIN_GREEN, LOW);
+
+	}
 
 }
 
